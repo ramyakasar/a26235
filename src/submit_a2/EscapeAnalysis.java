1d35c83ffa1ed1ed043c271a342803897dfeefe1
Ramya Kasaraneni <ramyakasaraneni@Ramyas-MacBook-Pro.local>
2023-03-13 11:09:05 +0530

    first commit

---
+++ src/submit_a2/EscapeAnalysis.java
@@ -0,0 +1,19 @@
+package submit_a2;
+
+import java.util.Map;
+
+import soot.Body;
+import soot.BodyTransformer;
+import soot.SceneTransformer;
+
+public class EscapeAnalysis extends SceneTransformer{
+
+	@Override
+	protected void internalTransform(String phaseName, Map<String, String> options) {
+		/*
+		 * Implement your escape analysis here
+		 */
+		
+	}
+	
+}
